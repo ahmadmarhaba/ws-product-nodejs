3a75739a5b89c45881483541a7fa6f96b4e1128d
ahmadmarhaba <dragon.8881@hotmail.com>
2022-08-07 01:56:17 +0300

    Update redis.js

--- redis.js
+++ redis.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 const Redis = require('ioredis');
-const redis = new Redis(`redis://${process.env.REDIS_USER}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_ENDPOINT_URI}:${process.env.REDIS_ENDPOINT_PORT}/0`)// <ignore scan-env>
+const redis = new Redis(`redis://${process.env.REDIS_USER}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_ENDPOINT_URI}/0`)// <ignore scan-env>
 
 module.exports = redis ? redis : null;
